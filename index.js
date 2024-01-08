583c7d65b827f640b71b02b094baa0a912add731
Suthesh pravin.v <sutheshpravin4@gmail.com>
2024-01-08 11:27:00 +0530

    updated mainpage

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
 
-let a="10";
-let b="10";
 function upcheck()
 {
-     a = document.getElementById("pass").value;
-     b = document.getElementById("user").value;
+     b = document.getElementById("pass").value;
+     a = document.getElementById("user").value;
     console.log(a);
     console.log(b);
-    if(b==="Someone@gmail.com"||a==="1234")
+    if(a==="Someone@gmail.com"||b==="1234")
      {
          alert("You entered a correct password");
-         
+         window.location.href="https://www.youtube.com";
         }
         else{
             alert("You entered a wrong password");
-            window.location.href="mainpage.html";
         // window.close();
-        // window.open("https://www.youtube.com");
-              }
+        // window.open("https://www.youtube.com");              }
 
-}
+}   };
 let pop="Someone@gmail.com";
 console.log("Someone@gmail.com"==pop);
 console.log(a);
